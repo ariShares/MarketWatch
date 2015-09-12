f0cfcabc0c0581e06fcd9ee916d78ab9b9bc6f87
Ellis Bigelow <ellisbigelow@gmail.com>
2015-09-12 15:19:09 -0700

    Fixed gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,37 +3,52 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
+    jshint: {
+      options: {
+        reporter: require('jshint-stylish') // use jshint-stylish to make our errors look and read good
+      },
+      files: ['Gruntfile.js', 'src/**/*.js']
+    },
+
+    concat: {
+      options: {
+        separator: ';'
+      },
+      build: {
+        src: ['src/**/*.js'],
+        dest: 'build/app.min.js'
+      }
+    },
+
     uglify: {
       options: {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      }
+      },
+      build: {
+        src: 'build/app.min.js',
+        dest: 'build/app.min.js'
+      },
     },
-    concat: {
-      option: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      }
-    },
-    build: {
-      src: 'src/*.js',
-      dest: 'build/app.min.js'
-    },
+
     watch: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      files: ['<%= jshint.files %>'],
+      tasks: ['default']
     }
   });
 
   // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
-  // Default task(s).
   grunt.registerTask('default', '', function () {
     var taskList = [
-    'uglify',
-    'concat',
-    'build',
-    'watch'
+      'jshint',
+      'concat',
+      'uglify',
+      'watch'
     ];
     grunt.task.run(taskList);
   });
--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ app.get('/', function(req, res) {
   res.send('hello world');
 });
 
-
 var server = app.listen(8000, function () {
   var host = server.address().address;
   var port = server.address().port;
